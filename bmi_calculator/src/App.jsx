f0dde671702dd5b7c8732e00ed617c7cb69d96a2
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-14 13:57:43 +0200

    I am finishing the cleaning up the src/App.js by turning it into a jsx file, I am creating the integration folder in directory cypress, I am adding a test folder in src and in that folder I am creating the first tests in usercalculatetheirbmi.spec.js. I am also creating input fields for weight height in app.jsx, and I am creating a component test for the Form component in Form.spec.js.

---
+++ bmi_calculator/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+
+ class App extends Component {
+   render() {
+     return (
+       <form>
+         <label htmlFor="weight">Weight</label>
+         <input 
+         type="number"
+         required
+         placeholder="Weight in kgs"
+         name="weight"
+         id="weight"
+         />
+        <label htmlFor="height">Height</label>
+        <input 
+        type="number"
+        required
+        placeholder
+        name="height"
+        id="height"
+        />
+        <button>Calculate BMI</button>
+       </form>
+     );
+    }
+  }
+ 
+
+export default App;
